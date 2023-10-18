aeb050aeec3cde4e9db436c914b2eefb461dc578
Alex Matte <alex.matte71@gmail.com>
2023-10-18 21:36:31 +0200

    created basic 404 page for correct route testing

---
+++ controllers/404.php
@@ -0,0 +1,5 @@
+<?php
+
+$header = "Something went wrong...";
+
+require view("404.view.php");
---
+++ views/404.view.php
@@ -0,0 +1,12 @@
+<?php require('partials/header.php') ?>
+<?php require('partials/nav.php') ?>
+<?php require('partials/banner.php') ?>
+
+
+<main>
+    <div>
+        <h5>404 Page not found!</h5>
+    </div>
+</main>
+
+<?php require('partials/footer.php') ?>
