97078c856b12fcd3d545daa688a8fa4a210f6b17
alex <alex.matte71@gmail.com>
2023-10-30 00:14:45 +0100

    added journal link to navbar

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -20,10 +20,13 @@
       <!-- Left links -->
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link" href="/">Home</a>
+            <a class="nav-link" href="/">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/contact">Contact</a>
+            <a class="nav-link" href="/contact">Contact</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="/journal">Journal</a>
         </li>
       </ul>
       <!-- Left links -->
