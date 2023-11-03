b07738ca104bc5137eaaadd78ad383b12968f142
Alexander Matte <alex.matte71@gmail.com>
2023-11-03 22:59:17 +0100

    changes input to textarea

--- views/journal.view.php
+++ views/journal.view.php
@@ -13,7 +13,7 @@
                     <form method="post">
                         <div class="mb-3">
                             <label for="journal_body" class="form-label">Your Journal Entry:</label>
-                            <input type="text" class="form-control" id="journal_body" name="journal_body" placeholder="Today was a beautiful day because... Today was shit because...">
+                            <textarea class="form-control" id="journal_body" name="journal_body" rows="10" cols="5" placeholder="Today was a beautiful day because... Today was shit because..."></textarea>
                         </div>
                         <button type="submit" class="btn btn-primary">Submit</button>
                     </form>
