1c42ea0f525f849e5d56779802cdc7f01587c248
Alexander Matte <alex.matte71@gmail.com>
2023-11-03 23:01:21 +0100

    Created config for DB class

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'database' => [
+        'host' => '127.0.0.1',
+        'user' => 'root',
+        'password' => 'root',
+        'port' => 3306,
+        'dbname' => 'journal',
+        'charset' => 'utf8mb4'
+    ],
+
+    //
+];
