87a046e9a02bcc27e9fc56895bdd0c5c8f50c858
Alexander Matte <alex.matte71@gmail.com>
2023-11-04 23:08:05 +0100

    added to DB class to find specific item or all items

--- Core/Database.php
+++ Core/Database.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Core;
+use mysql_xdevapi\Statement;
 use PDO;
 use PDOException;
 
@@ -9,23 +10,46 @@ class Database
 {
     public $config;
     public $dsn;
-    public $db;
+    public $connection;
+    public $statement;
     public function __construct()
     {
         $this->config = require base("config.php");
 
-        $this->dsn = "mysql:host={$this->config['database']['host']};port=3306;{$this->config['database'] ['dbname']};charset=UTF8";
-        //
-        try {
-            $pdo = new PDO($this->dsn, $this->config['database']['user'], $this->config['database']['password']);
+        $this->dsn = "mysql:host={$this->config['database']['host']};port=3306;dbname={$this->config['database']['dbname']};charset=UTF8";
 
-            if ($pdo) {
-                $this->db = $pdo;
-                echo "Connected to the {$this->config['database']['dbname']} database successfully!";
-            }
-        } catch (PDOException $e) {
-            echo $e->getMessage();
+        $this->connection = new PDO($this->dsn, $this->config['database']['user'], $this->config['database']['password'], [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
+    }
+
+    public function query($query, $params = [])
+    {
+        $this->statement = $this->connection->prepare($query);
+
+        $this->statement->execute($params);
+
+        return $this;
+    }
+    public function findAll()
+    {
+        return $this->statement->fetchAll();
+    }
+
+    public function find()
+    {
+        return $this->statement->fetch();
+    }
+
+    public function findOrFail()
+    {
+        $result = $this->find();
+
+        if (! $result) {
+            echo "No result has been found!";
         }
+
+        return $result;
     }
 
 }
