beef7ecd781e737c5f8fcc834513b9db22fcd3a9
“Alexander <alex.matte71@gmail.com>
2023-11-19 17:23:06 +0100

    added redirect to 403 page

--- controllers/journal/entry.php
+++ controllers/journal/entry.php
@@ -5,10 +5,18 @@ use Core\Database;
 $header = "Journal Entry";
 
 $db = new Database();
+//Temporary hardcoded while figuring out authentication
 $userLoggedIn = 4;
+
+if($userLoggedIn != $_GET["id"])
+{
+    require base("controllers/403.php");
+    exit();
+}
 $result = $db->query("SELECT * FROM `entries` WHERE id = :id", [
     "id" => $_GET["id"]
 ])->findOrFail();
+
 
 
 
