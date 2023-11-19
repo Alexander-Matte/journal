0e8a133f9d43a09d07d4617952aee6c0212f9db2
“Alexander <alex.matte71@gmail.com>
2023-11-19 17:22:17 +0100

    created logic to add entries to DB

--- controllers/journal/create.php
+++ controllers/journal/create.php
@@ -3,9 +3,25 @@
 use Core\Database;
 
 $header = "Create a journal entry";
+//Temp hard coded until authentication is built
+$user = 1;
 
 $db = new Database();
+if(isset($_POST["content"]))
+{
+    $title = $_POST["title"];
+    $content = $_POST["content"];
+    $result = $db->query("INSERT INTO entries (user_id, title, content)
+    VALUES ($user, '$title', '$content')");
+}
 
 
 
 require view("journal/create.view.php");
+
+
+
+
+
+
+
--- views/journal/create.view.php
+++ views/journal/create.view.php
@@ -13,8 +13,12 @@
                         <div class="col-md-6">
                             <form method="post">
                                 <div class="mb-3">
-                                    <label for="journal_body" class="form-label">Your Journal Entry:</label>
-                                    <textarea class="form-control" id="journal_body" name="journal_body" rows="10" cols="5" placeholder="Today was a beautiful day because... Today was shit because..."></textarea>
+                                    <label for="title" class="form-label">Title:</label>
+                                    <input type="text" class="form-control" id="title" name="title" placeholder="Enter a title for your journal entry">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="content" class="form-label">Your Journal Entry:</label>
+                                    <textarea class="form-control" id="content" name="content" rows="10" cols="5" placeholder="Today was a beautiful day because... Today was shit because..."></textarea>
                                 </div>
                                 <button type="submit" class="btn btn-primary">Submit</button>
                             </form>
