66c8b91223dc71fe0695544bba65a668aeb61f3e
“Alexander <alex.matte71@gmail.com>
2023-11-19 17:26:44 +0100

    created 403 page for unauthorized visits

---
+++ views/403.view.php
@@ -0,0 +1,9 @@
+<?php require('partials/header.php') ?>
+<?php require('partials/nav.php') ?>
+<?php require('partials/banner.php') ?>
+
+
+    <main class="ms-5">
+    </main>
+
+<?php require('partials/footer.php') ?>
