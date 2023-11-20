3563fb718c37d103a617cd209b4f5e04235f1f8a
“Alexander <alex.matte71@gmail.com>
2023-11-20 21:14:58 +0100

    queried DB to find if user exists

--- controllers/login/index.php
+++ controllers/login/index.php
@@ -2,4 +2,21 @@
 
 $header = "Log in";
 
-require view("login/create.view.php");
+$db = new Core\Database();
+
+if(isset($_POST["password"]))
+{
+    $result = $db->query("SELECT * FROM `users` WHERE email = :email AND password = :password", [
+        "email" => $_POST["email"],
+        "password" => $_POST["password"]
+    ])->findOrFail();
+}
+
+$_SESSION["user"] = $result["id"];
+
+var_dump($_SESSION);
+
+
+
+
+require view("login/index.view.php");
