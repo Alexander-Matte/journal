d988b1505fbeee5974b0c34b53febe3ae0cfbca0
“Alexander <alex.matte71@gmail.com>
2023-11-20 21:16:42 +0100

    created login form view

---
+++ views/login/index.view.php
@@ -0,0 +1,34 @@
+<?php require(base("views/partials/header.php")) ?>
+<?php require(base("views/partials/nav.php")) ?>
+<?php require(base("views/partials/banner.php")) ?>
+
+
+<main>
+    <div>
+        <p class="ms-5">This page will be used to display login form</p>
+    </div>
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h2 class="card-title text-center mb-4">Login</h2>
+                        <form method="post">
+                            <div class="form-group">
+                                <label for="email">Email</label>
+                                <input type="text" class="form-control" id="email" name="email" placeholder="Enter email">
+                            </div>
+                            <div class="form-group mb-3">
+                                <label for="password">Password</label>
+                                <input type="password" class="form-control" id="password" name="password" placeholder="Enter password">
+                            </div>
+                            <button type="submit" class="btn btn-primary btn-block">Login</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+
+<?php require(base('views/partials/footer.php')) ?>
