63d7e9802b5cbb6ddcff1e07c075a4938f7fd34b
“Alexander <alex.matte71@gmail.com>
2023-11-20 21:15:48 +0100

    added composer autoloader logic and session start

--- public/index.php
+++ public/index.php
@@ -1,22 +1,18 @@
 <?php
-
-
-use Core\Router;
-
 ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
-
-
 const BASE_PATH = __DIR__ . '/../';
-
 require BASE_PATH . "Core/functions.php";
-require base("Core/Router.php");
+require BASE_PATH . '/vendor/autoload.php';
+
+session_start();
+
+
 require base("routes.php");
-require base("Core/Database.php");
 
-$uri = parse_url($_SERVER["REQUEST_URI"]);
+$uri = parse_url($_SERVER["REQUEST_URI"])["path"];
 
 
-$router = new Router($routes);
-$router->route($uri["path"]);
+$router = new Core\Router($routes);
+$router->route($uri);
 
 ?>
