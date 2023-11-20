3c90644a8546b9c73891fe6934b857be7e4d4008
“Alexander <alex.matte71@gmail.com>
2023-11-20 21:14:03 +0100

    created logic for adding journal entries to DB

--- controllers/journal/create.php
+++ controllers/journal/create.php
@@ -1,23 +1,23 @@
 <?php
 
-use Core\Database;
-
 $header = "Create a journal entry";
 //Temp hard coded until authentication is built
 $user = 1;
 
-$db = new Database();
+$db = new Core\Database();
 if(isset($_POST["content"]))
 {
-    $title = $_POST["title"];
-    $content = $_POST["content"];
     $result = $db->query("INSERT INTO entries (user_id, title, content)
-    VALUES ($user, '$title', '$content')");
+    VALUES (:userId, :title, :content)", [
+        'userId' => $user,
+        'title' => $_POST["title"],
+        'content' => $_POST["content"]
+    ]);
 }
 
 
 
-require view("journal/create.view.php");
+require view("journal/index.view.php");
 
 
 
