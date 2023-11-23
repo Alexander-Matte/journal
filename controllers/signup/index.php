6e8b55f6f5318121b44d4cf9db91fcab2d7a1670
“Alexander <alex.matte71@gmail.com>
2023-11-23 20:39:15 +0100

    created sign up page logic to sign new users up

--- controllers/signup/index.php
+++ controllers/signup/index.php
@@ -1,5 +1,25 @@
 <?php
-
+use Core\Authenticator;
+use Core\Database;
 $header = "Sign up";
+
+if(isset($_POST["email"]) && isset($_POST["password"]))
+{
+    if(Authenticator::email($_POST["email"]))
+    {
+        //Need to build errors and notify user that emails already in use
+        dd("EMAIL IN USE");
+    }
+
+    $db = new Database();
+    $db->addUser($_POST["email"],$_POST["password"],$_POST["firstName"],$_POST["lastName"]);
+    header("Location: /login");
+    exit();
+
+}
+
+
+
+
 
 require view("signup/index.view.php");
