011a722d8c95bb6a212df6367c8588cfae8dba70
“Alexander <alex.matte71@gmail.com>
2023-11-23 17:48:57 +0100

    renamed variable

--- controllers/journal/create.php
+++ controllers/journal/create.php
@@ -2,14 +2,14 @@
 
 $header = "Create a journal entry";
 //Temp hard coded until authentication is built
-$user = 1;
+$userId = 1;
 
 $db = new Core\Database();
 if(isset($_POST["content"]))
 {
     $result = $db->query("INSERT INTO entries (user_id, title, content)
     VALUES (:userId, :title, :content)", [
-        'userId' => $user,
+        'userId' => $userId,
         'title' => $_POST["title"],
         'content' => $_POST["content"]
     ]);
@@ -17,7 +17,7 @@ if(isset($_POST["content"]))
 
 
 
-require view("journal/index.view.php");
+require view("journal/create.view.php");
 
 
 
