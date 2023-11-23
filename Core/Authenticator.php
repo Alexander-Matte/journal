1af88b28afe0e4122491ca22ac0ffb248ec322a0
“Alexander <alex.matte71@gmail.com>
2023-11-23 17:48:27 +0100

    created basic authenticator to check if user exists in DB

---
+++ Core/Authenticator.php
@@ -0,0 +1,17 @@
+<?php
+namespace Core;
+
+
+class Authenticator
+{
+    public static function authenticateUser($userEmail, $password)
+    {
+        $db = new Database();
+        //Check database if user exists
+        return $db->query("SELECT * FROM `users` WHERE email = :email AND password = :password", [
+            "email" => $userEmail,
+            "password" => $password
+        ])->findOrFail();
+
+    }
+}
