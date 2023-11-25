508744b2561359348ef94685668f514cf40c99e7
“Alexander <alex.matte71@gmail.com>
2023-11-25 21:08:58 +0100

    added temp jquery cdn

--- views/partials/header.php
+++ views/partials/header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://kit.fontawesome.com/04ed09ad32.js" crossorigin="anonymous"></script>
     <title>My Journal</title>
 </head>
