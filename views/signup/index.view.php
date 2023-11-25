53be665e12445c511369f4d2e43361169446b462
“Alexander <alex.matte71@gmail.com>
2023-11-25 21:11:34 +0100

    added user feedback for email/password errors on signup

--- views/signup/index.view.php
+++ views/signup/index.view.php
@@ -23,20 +23,52 @@
                             </div>
                             <div class="form-group">
                                 <label for="email">Email</label>
-                                <input type="email" class="form-control" id="email" name="email" placeholder="Enter email" required>
+                                <input type="email" class="form-control" id="email" name="email" placeholder="Enter email">
                             </div>
                             <div class="form-group mb-3">
                                 <label for="password">Password</label>
                                 <input type="password" class="form-control" id="password" name="password" placeholder="Enter password" required>
+                                <div id="passwordHelp" class="form-text text-danger"></div>
                             </div>
+
+
                             <button type="submit" class="btn btn-primary btn-block">Sign Up</button>
                             <small class="form-text text-muted mt-2">Note: First Name and Last Name are optional fields.</small>
                         </form>
+                        <?php if(isset($errors)): ?>
+                            <?php foreach($errors as $error): ?>
+                                <ul class="list-unstyled">
+                                    <li class="alert alert-danger">
+                                        <?= $error; ?>
+                                    </li>
+                                </ul>
+                            <?php endforeach; ?>
+                        <?php endif ?>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </main>
+<script>
+    document.getElementById('password').addEventListener('focus', function () {
+        var passwordHelp = document.getElementById('passwordHelp');
+        passwordHelp.innerHTML = 'Password must:';
+        passwordHelp.innerHTML += '<ul>';
+        passwordHelp.innerHTML += '<li id="length">Have minimum 8 characters in length.</li>';
+        passwordHelp.innerHTML += '<li id="uppercase">Contain at least one uppercase English letter.</li>';
+        passwordHelp.innerHTML += '<li id="lowercase">Contain at least one lowercase English letter.</li>';
+        passwordHelp.innerHTML += '<li id="digit">Contain at least one digit.</li>';
+        passwordHelp.innerHTML += '</ul>';
+        passwordHelp.style.display = 'block';
+    });
+
+    document.getElementById('password').addEventListener('blur', function () {
+        var passwordHelp = document.getElementById('passwordHelp');
+        passwordHelp.style.display = 'none';
+    });
+</script>
+
+
 
 <?php require(base('views/partials/footer.php')) ?>
