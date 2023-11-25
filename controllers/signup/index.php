febb01fcd3d330c5a467d6912f5751889001bab4
“Alexander <alex.matte71@gmail.com>
2023-11-25 21:10:07 +0100

    added validation for email and password entries

--- controllers/signup/index.php
+++ controllers/signup/index.php
@@ -1,25 +1,44 @@
 <?php
 use Core\Authenticator;
 use Core\Database;
+use Core\Validator;
 $header = "Sign up";
+$errors = [];
 
 if(isset($_POST["email"]) && isset($_POST["password"]))
 {
+    // if email already exists in DB
     if(Authenticator::email($_POST["email"]))
     {
-        //Need to build errors and notify user that emails already in use
-        dd("EMAIL IN USE");
+        $errors = array_push($errors, "Email already in use");
+        require view("signup/index.view.php");
+        exit();
+    }
+    $email = Validator::validateEmail($_POST["email"]);
+    if(is_array($email))
+    {
+        $errors = $email;
+        require view("signup/index.view.php");
+        exit();
+    }
+
+    $result = Validator::validatePassword($_POST["password"]);
+
+    if(is_array($result))
+    {
+        $errors = $result;
+        require view("signup/index.view.php");
+        exit();
     }
 
     $db = new Database();
     $db->addUser($_POST["email"],$_POST["password"],$_POST["firstName"],$_POST["lastName"]);
-    header("Location: /login");
+    //TODO: Notify user of successful login
+    header("Location: http://localhost:7171/login");
     exit();
 
+
+
 }
-
-
-
-
 
 require view("signup/index.view.php");
