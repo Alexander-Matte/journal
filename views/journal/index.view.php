b9d52bb2f720f3b72e242d0f03c29494a338ee6d
“Alexander <alex.matte71@gmail.com>
2023-11-28 21:00:55 +0100

    made check if variable is available or not

--- views/journal/index.view.php
+++ views/journal/index.view.php
@@ -7,20 +7,22 @@
         <div class="row">
             <div class="col-lg-8">
                 <div class="mb-4">
-                    <a href="/journal/create" class="btn btn-primary btn-lg btn-block mb-4">Create a Journal Entry</a>
+                    <a href="/journal/create" class="btn btn-primary btn-md btn-block mb-4">Create a Journal Entry</a>
                 </div>
-                <?php foreach($results as $key => $value): ?>
-                    <div class="card mb-3">
-                        <div class="card-body">
-                            <h5 class="card-title">
-                                <a href=<?= "journal/show?id={$value["id"]}" ?>>
-                                    <?= $value["title"]; ?>
-                                </a>
-                            </h5>
-                            <p class="card-text"><?= $value["content"]; ?></p>
+                <?php if(isset($results)): ?>
+                    <?php foreach($results as $key => $value): ?>
+                        <div class="card mb-3">
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    <a href=<?= "journal/show?id={$value["id"]}" ?>>
+                                        <?= $value["title"]; ?>
+                                    </a>
+                                </h5>
+                                <p class="card-text"><?= $value["content"]; ?></p>
+                            </div>
                         </div>
-                    </div>
-                <?php endforeach; ?>
+                    <?php endforeach; ?>
+                <?php endif; ?>
             </div>
         </div>
     </main>
