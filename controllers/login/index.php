6048236c45f5cabc17c0fe7cf41d3e80fa28de75
“Alexander <alex.matte71@gmail.com>
2023-11-28 21:00:02 +0100

    created errors to display. Improved session logic

--- controllers/login/index.php
+++ controllers/login/index.php
@@ -1,23 +1,30 @@
 <?php
 use \Core\Authenticator;
+use \Core\Session;
+
 $header = "Log in";
+$errors = [];
 
+if (Session::has("errors")) {
+    $errors = Session::get("errors");
+    Session::unflash();
+}
 
-if(isset($_POST["password"]))
-{
-    //todo: notify user of authentication errors
-    if(Authenticator::authenticateUser($_POST["email"], $_POST["password"]))
-    {
+if (isset($_POST["password"])) {
+    $authenticated = Authenticator::authenticateUser($_POST["email"], $_POST["password"]);
 
-        $user = Authenticator::authenticateUser($_POST["email"], $_POST["password"]);
-        logIn($user);
+    if (!$authenticated) {
+        $error = "No user found with given username and password!";
+        Session::flash("errors", $error); // Flash the error message
+        header("Location: /login");
+        exit();
+    } else {
+        $user = Authenticator::authenticateUser($_POST["email"], $_POST["password"]); // Retrieve user details
+        Session::unflash();
+        Session::set("userId", $user["id"]); // Set user details in the session
         header("Location: /");
         exit();
     }
-
-    header($_SERVER["HTTP_REFERER"]);
-
 }
-
 
 require view("login/index.view.php");
