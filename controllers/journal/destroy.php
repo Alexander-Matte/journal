2adf1160978f5363d1a9c26e3438ff46d07d9765
“Alexander <alex.matte71@gmail.com>
2023-11-28 20:59:01 +0100

    created destroy script to remove entry from DB

--- controllers/journal/destroy.php
+++ controllers/journal/destroy.php
@@ -1,1 +1,20 @@
 <?php
+
+if(!isset($_SESSION["userId"]))
+{
+    require base("controllers/403.php");
+    exit();
+}
+$userId = $_SESSION["userId"];
+$postId = $_GET["id"];
+
+
+$db = new Core\Database();
+
+$result = $db->query("DELETE FROM `entries` WHERE user_id = :id AND id = :postId", [
+    "id" => $userId,
+    "postId" => $postId
+]);
+
+header("Location: /journal");
+exit();
