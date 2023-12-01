1389f1b92d2c7426aeaa6123eb34ad3a55713f27
“Alexander <alex.matte71@gmail.com>
2023-12-02 00:54:14 +0100

    added error messages for login submission

--- controllers/login/index.php
+++ controllers/login/index.php
@@ -4,27 +4,37 @@ use \Core\Session;
 
 $header = "Log in";
 $errors = [];
+$successRegistration = false;
 
 if (Session::has("errors")) {
     $errors = Session::get("errors");
     Session::unflash();
 }
 
-if (isset($_POST["password"])) {
-    $authenticated = Authenticator::authenticateUser($_POST["email"], $_POST["password"]);
+if(Session::has("reg_status")) {
+    $successRegistration = true;
+    $message = "Registration was successful! Please log in below!";
+    Session::unflash();
+}
+if($_SERVER['REQUEST_METHOD'] === "POST") {
+    if (!empty($_POST["password"])) {
+        $authenticated = Authenticator::authenticateUser($_POST["email"], $_POST["password"]);
 
-    if (!$authenticated) {
-        $error = "No user found with given username and password!";
-        Session::flash("errors", $error); // Flash the error message
-        header("Location: /login");
-        exit();
-    } else {
+        if (!$authenticated) {
+            $error = "No user found with given username and password!";
+            Session::flash("errors", $error); // Flash the error message
+            header("Location: /login");
+            exit();
+        }
+
         $user = Authenticator::authenticateUser($_POST["email"], $_POST["password"]); // Retrieve user details
         Session::unflash();
         Session::set("userId", $user["id"]); // Set user details in the session
         header("Location: /");
         exit();
+
     }
 }
+
 
 require view("login/index.view.php");
