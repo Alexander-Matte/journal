3aaf0b60c123d50036b8d3fde78803c42de607e2
“Alexander <alex.matte71@gmail.com>
2023-12-04 21:39:49 +0100

    added password hash check when authenticating user

--- Core/Authenticator.php
+++ Core/Authenticator.php
@@ -4,15 +4,20 @@ namespace Core;
 
 class Authenticator
 {
-    public static function authenticateUser($userEmail, $password)
+    public static function authenticateUser($email, $password)
     {
         $db = new Database();
         //Check database if user exists
-        return $db->query("SELECT * FROM `users` WHERE email = :email AND password = :password", [
-            "email" => $userEmail,
-            "password" => $password
+        $result = $db->query("SELECT * FROM `users` WHERE email = :email", [
+            "email" => $email,
         ])->findOrFail();
 
+        if(!password_verify($password, $result['password']))
+        {
+            return false;
+        }
+
+       return $result;
     }
 
     public static function email($email)
