e25af4bcc96a2439de1ff970a96f41d4b649097c
“Alexander <alex.matte71@gmail.com>
2023-12-18 22:01:23 +0100

    moved to web/views

---
+++ web/views/httpResponseStatus.view.php
@@ -0,0 +1,7 @@
+<?php require('partials/header.view.php') ?>
+<?php require('partials/nav.view.php') ?>
+<?php require('partials/banner.view.php') ?>
+
+
+
+<?php require('partials/footer.view.php') ?>
---
+++ web/views/index.view.php
@@ -0,0 +1,48 @@
+<?php require('partials/header.view.php') ?>
+<?php require('partials/nav.view.php') ?>
+
+<main>
+    <div class="container">
+
+        <div class="container">
+            <h1 class="text-center mb-4">The Importance and Benefits of Daily Journaling</h1>
+
+            <div class="benefit">
+                <h3>1. Reflective Practice</h3>
+                <p>Journaling daily allows you to reflect on your thoughts, actions, and experiences, enabling deeper self-awareness and personal growth.</p>
+            </div>
+
+            <div class="benefit">
+                <h3>2. Stress Reduction</h3>
+                <p>Writing down your feelings and emotions can help reduce stress, as it provides an outlet to express and process your thoughts.</p>
+            </div>
+
+            <div class="benefit">
+                <h3>3. Goal Setting and Tracking</h3>
+                <p>Journaling aids in setting and tracking goals, making it easier to identify progress and adjust strategies towards achieving them.</p>
+            </div>
+
+            <div class="benefit">
+                <h3>4. Creative Outlet</h3>
+                <p>It serves as a creative outlet, allowing you to explore ideas, sketch, or jot down inspirations without constraints.</p>
+            </div>
+
+            <div class="benefit">
+                <h3>5. Memory Improvement</h3>
+                <p>Regularly writing about your experiences helps in memory retention and cognitive processes, enhancing overall memory.</p>
+            </div>
+
+            <div class="benefit">
+                <h3>6. Emotional Intelligence</h3>
+                <p>Journaling can enhance emotional intelligence by helping you recognize and regulate your emotions effectively.</p>
+            </div>
+
+            <div class="benefit">
+                <h3>7. Personal Accountability</h3>
+                <p>Keeping a journal encourages personal accountability by reflecting on decisions and their consequences.</p>
+            </div>
+        </div>
+    </div>
+</main>
+
+<?php require('partials/footer.view.php') ?>
