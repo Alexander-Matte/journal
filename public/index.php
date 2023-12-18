07f6378ab137ec4dfe2d90ea71b250c1c99ac525
“Alexander <alex.matte71@gmail.com>
2023-12-18 22:02:05 +0100

    added constant for controller locations

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
 const BASE_PATH = __DIR__ . '/../';
+const CONTROLLER_PATH = 'web/controllers/';
 require BASE_PATH . "Core/functions.php";
 require BASE_PATH . '/vendor/autoload.php';
 use Core\Router;
